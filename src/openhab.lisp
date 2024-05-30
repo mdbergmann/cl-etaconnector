93185742f80b03dc46a17710be1c0d50a1a749b5
Manfred Bergmann <mb@software-by-mabe.com>
2024-05-30 13:39:47 +0200

    copy data when double float.

--- src/openhab.lisp
+++ src/openhab.lisp
@@ -8,11 +8,13 @@
 (defparameter *openhab-base-url* "http://mini.local:8080/rest/items/")
 
 (defun do-post (resource data)
-  (when (typep data 'double-float)
-    (let ((data (coerce data 'single-float)))
-      (log:info "posting item: ~a, value: ~a" resource data)
-      (drakma:http-request (format nil "~a~a" *openhab-base-url* resource)
-			   :method :post
-			   :content (write-to-string data)
-			   :content-type "text/plain")))
+  (let ((post-data 
+	  (if (typep data 'double-float)
+	    (coerce data 'single-float)
+	    data)))
+    (log:info "posting item: ~a, value: ~a" resource post-data)
+    (drakma:http-request (format nil "~a~a" *openhab-base-url* resource)
+			 :method :post
+			 :content (write-to-string post-data)
+			 :content-type "text/plain"))
   :ok)
